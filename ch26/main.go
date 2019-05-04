38e065d0e7396191669853138a5bd0456c5906e2
ChongmingDu <decodedcm@gmail.com>
2019-05-04 21:30:31 +0800

    Add Leetcode #26.

---
+++ ch26/main.go
@@ -0,0 +1,15 @@
+package main
+
+func removeDuplicates(nums []int) int {
+	if len(nums) < 2 {
+		return len(nums)
+	}
+	count := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[count] != nums[i] {
+			count += 1
+			nums[count] = nums[i]
+		}
+	}
+	return count + 1
+}
